5603d0b8733d63e0733c46a4a2169b6daa36dbef
Wi11iamC <willi4m553@gmail.com>
2022-09-04 00:32:28 -0400

    Update ModalElements.tsx

--- src/Components/ModalElements.tsx
+++ src/Components/ModalElements.tsx
@@ -3,24 +3,17 @@ import React from "react";
 
 export default function ModalElements(props:any) {
   return (
-    <div id={`modal-${props.idx}`} className=" modal-main inactive">
+    <div id={`modal-${props.idx}`} className="modal-main inactive">
                 <h3>{props.room}</h3>
                 <button onClick={() => props.closeModal(props.idx)}>close</button>
                 {
-                props.coursesData.map((course:any) => (
+                props.coursesData.map((course:any, index:any) => (
                         
-                        course.sections.map((section:any) => (
-
-                        section.meetingTimes.map((classMeeting:any) => (
-                                <div key={section.index}>
-                                        {(classMeeting.roomNumber === props.room && 
-                                         <p>{course.title} | section: {section.number} | level: {course.level} | {`${classMeeting.startTime[0] == '0' ? classMeeting.startTime.substring(1).slice(0, 1) + ':' + classMeeting.startTime.substring(1).slice(1) : classMeeting.startTime.slice(0, 2) + ':' + classMeeting.startTime.slice(2)}${classMeeting.pmCode}M-${classMeeting.endTime[0] == '0' ? classMeeting.endTime.substring(1).slice(0, 1) + ':' + classMeeting.endTime.substring(1).slice(1) : classMeeting.endTime.slice(0, 2) + ':' + classMeeting.endTime.slice(2)}${classMeeting.endTimeMilitary.substring(0, 2) > '11' ? 'PM' : 'AM'}`} | {/* testing purposes room={classMeeting.roomNumber}*/} </p>
+                                <div key={index}>
+                                        {(course.room === props.room && 
+                                         <p>{course.title} | section: {course.sectionNumber} | level: {course.level} | {course.startTime}-{course.endTime} | mode: {course.mode}</p>
                                          )}
                                 </div>
-
-                        ))
-
-                ))
                         
 
                 ))
