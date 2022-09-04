92fa547b3423f79975a061c7fe78fa5533199703
Wi11iamC <willi4m553@gmail.com>
2022-09-04 09:25:53 -0400

    Update extractData.ts

--- src/extractData.ts
+++ src/extractData.ts
@@ -13,37 +13,41 @@ export default function extractData(coursesData:any, day:any, building:any):Arra
 
     let res:Array<courseObject> =[];
 
+    console.log(coursesData);
+    
 
 coursesData.forEach((course:any) => {
 
-    let classObject:courseObject = {
-        title: "",
-        sectionNumber: "",
-        startTime: "",
-        endTime: "",
-        level: "",
-        courseString: "",
-        room: "",
-        mode: "",
-    };
 
-    classObject.title = course.title;
-    classObject.courseString = course.courseString;
-    classObject.level = course.level;
 
     course.sections.forEach((section:any) => {
         
-        classObject.sectionNumber = section.number;
 
             section.meetingTimes.forEach((classMeeting:any) => {
                 if (classMeeting.meetingDay === day && classMeeting.buildingCode === building) {
-                    console.log(classObject.title ,classObject.sectionNumber);
-                    
+                    let classObject:courseObject = {
+                        title: "",
+                        sectionNumber: "",
+                        startTime: "",
+                        endTime: "",
+                        level: "",
+                        courseString: "",
+                        room: "",
+                        mode: "",
+                    };
+                    classObject.title = course.title;
+                    classObject.courseString = course.courseString;
+                    classObject.level = course.level;
+                    classObject.sectionNumber = section.number;
                 classObject.startTime = `${classMeeting.startTime[0] == '0' ? classMeeting.startTime.substring(1).slice(0, 1) + ':' + classMeeting.startTime.substring(1).slice(1) : classMeeting.startTime.slice(0, 2) + ':' + classMeeting.startTime.slice(2)}${classMeeting.pmCode}M`
                 classObject.endTime = `${classMeeting.endTime[0] == '0' ? classMeeting.endTime.substring(1).slice(0, 1) + ':' + classMeeting.endTime.substring(1).slice(1) : classMeeting.endTime.slice(0, 2) + ':' + classMeeting.endTime.slice(2)}${classMeeting.endTimeMilitary.substring(0, 2) > '11' ? 'PM' : 'AM'}`;
                 classObject.room = classMeeting.roomNumber;
                 classObject.mode = classMeeting.meetingModeDesc;
                 res.push(classObject); 
+                console.log(classObject);
+                
+                console.log(res);
+                
             }
             })
     })
