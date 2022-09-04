6c80917ba01e6607430083746b12d198d017bc4f
Wi11iamC <willi4m553@gmail.com>
2022-09-04 09:06:40 -0400

    Update extractData.ts

--- src/extractData.ts
+++ src/extractData.ts
@@ -32,9 +32,13 @@ coursesData.forEach((course:any) => {
     classObject.level = course.level;
 
     course.sections.forEach((section:any) => {
+        
         classObject.sectionNumber = section.number;
+
             section.meetingTimes.forEach((classMeeting:any) => {
                 if (classMeeting.meetingDay === day && classMeeting.buildingCode === building) {
+                    console.log(classObject.title ,classObject.sectionNumber);
+                    
                 classObject.startTime = `${classMeeting.startTime[0] == '0' ? classMeeting.startTime.substring(1).slice(0, 1) + ':' + classMeeting.startTime.substring(1).slice(1) : classMeeting.startTime.slice(0, 2) + ':' + classMeeting.startTime.slice(2)}${classMeeting.pmCode}M`
                 classObject.endTime = `${classMeeting.endTime[0] == '0' ? classMeeting.endTime.substring(1).slice(0, 1) + ':' + classMeeting.endTime.substring(1).slice(1) : classMeeting.endTime.slice(0, 2) + ':' + classMeeting.endTime.slice(2)}${classMeeting.endTimeMilitary.substring(0, 2) > '11' ? 'PM' : 'AM'}`;
                 classObject.room = classMeeting.roomNumber;
