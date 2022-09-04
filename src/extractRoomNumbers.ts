5ade9d388cbfdd7eefa89832522c24ada146b330
Wi11iamC <willi4m553@gmail.com>
2022-09-04 00:32:46 -0400

    Create extractRoomNumbers.ts

---
+++ src/extractRoomNumbers.ts
@@ -0,0 +1,16 @@
+export default function extractRoomNumbers(data: any): Array<string>{
+
+    let res: Array<string> = [];
+if (data.length > 1) {
+    data.forEach((course:any) => {
+
+                if (!res.some((room:string) => (room === course.room))) {
+                    res.push(course.room);
+                }
+
+
+    })
+}
+    return res;
+    
+}
