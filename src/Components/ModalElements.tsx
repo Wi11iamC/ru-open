73881828d49bbbd6894e920fd5af38751a2e08e8
Wi11iamC <willi4m553@gmail.com>
2022-09-04 23:25:08 -0400

    Update ModalElements.tsx

--- src/Components/ModalElements.tsx
+++ src/Components/ModalElements.tsx
@@ -5,11 +5,11 @@ export default function ModalElements(props:any) {
   return (
     <div id={`modal-${props.idx}`} className="modal-main inactive">
                 <h3>{props.room}</h3>
-                <button onClick={() => props.closeModal(props.idx)}>close</button>
+                <button className="close-modal-button" onClick={() => props.closeModal(props.idx)}>Close</button>
                 {
                 props.coursesData.map((course:any, index:any) => (
                         
-                                <div key={index}>
+                                <div key={index} className="modal-course-description">
                                         {(course.room === props.room && 
                                          <p>{course.title} | {course.courseString} | section: {course.sectionNumber} | level: {course.level} | {course.startTime}-{course.endTime} | mode: {course.mode}</p>
                                          )}
